80928f93c3bf733742c5675dab43f0b154d84f6b
realzaidiguy <realzaidiguy@gmail.com>
2017-12-03 11:29:57 -0500

    completed part 2 of day 1

--- day1/day1-part1.js
+++ day1/day1-part1.js
@@ -1,4 +1,4 @@
-// Day 1 challenge - Details/Instructions at the bottom
+// Day 1 Part 1 challenge - Details/Instructions at the bottom
 // Run this file via node
 
 // First set the captcha to be solved
@@ -33,6 +33,7 @@ console.log(count);
 http://adventofcode.com/2017/day/1
 
 --- Day 1: Inverse Captcha ---
+--- Part One ---
 
 The night before Christmas, one of Santa's Elves calls you in a panic. "The printer's broken! We can't print the Naughty or Nice List!" By the time you make it to sub-basement 17, there are only a few minutes until midnight. "We have a big problem," she says; "there must be almost fifty bugs in this system, but nothing else can print The List. Stand in this square, quick! There's no time to explain; if you can convince them to pay you in stars, you'll be able to--" She pulls a lever and the world goes blurry.
 
